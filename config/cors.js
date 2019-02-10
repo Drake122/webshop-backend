d75501821724b6a61e877e58f10d0bda86a12abd
Drake122 <anikokov124@gmail.com>
2019-02-10 18:57:46 +0100

    cors changes

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 
 module.exports.cors = {
   allRoutes: true,
-  origin: 'http://localhost:1337,http://localhost:3000',
+  origin: '89.134.52.104:1337,89.134.52.104:3000,http://localhost:1337,http://localhost:3000',
   headers: 'Content-Type, Authorization',
   credentials: false
 };
