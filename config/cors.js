8df56a9e83f4f57b6089e62bd58c41615716629a
Drake122 <anikokov124@gmail.com>
2019-03-04 23:05:47 +0100

    Add one cors url

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 
 module.exports.cors = {
   allRoutes: true,
-  origin: 'http://skovacs.ddns.net:81,http://skovacs.ddns.net:80,http://skovacs.ddns.net:82,http://localhost:1337,http://localhost:3000',
+  origin: 'http://skovacs.ddns.net:81,http://skovacs.ddns.net:80,http://skovacs.ddns.net:82,http://skovacs.ddns.net,http://localhost:1337,http://localhost:3000',
   headers: 'Content-Type, Authorization',
   credentials: false
 };
